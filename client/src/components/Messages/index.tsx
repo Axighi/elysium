b702d752b9b16100ff3ecf8512a179d8d6a95f8b
Azriel <xudong.liang@pwc.com>
2019-12-06 13:32:08 +0800

    delete message

--- client/src/components/Messages/index.tsx
+++ client/src/components/Messages/index.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import { useQuery, useMutation, useApolloClient } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
-import { List, Button } from "antd";
+import { List, Skeleton, Avatar } from "antd";
 
 const MESSAGES = gql`
   {
@@ -21,14 +21,27 @@ const CREATE_MESSAGE = gql`
   }
 `;
 
+const DELETE_MESSAGE = gql`
+  mutation deleteMessage($msg: MessageWhereUniqueInput!) {
+    deleteMessage(where: $msg) {
+      id
+      description
+    }
+  }
+`;
+
 const Messages: React.FC = () => {
-  const { loading, error, data } = useQuery(MESSAGES);
+  const { loading, error, data = { messages: [] } } = useQuery(MESSAGES);
   const [createMessage, createdResult] = useMutation(CREATE_MESSAGE);
+  const [deleteMessage, deletedResult] = useMutation(DELETE_MESSAGE);
 
   let input: any;
 
-  if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
+
+  const handleClickDelete = (id: string) => {
+    deleteMessage({ variables: { msg: { id } } });
+  };
 
   return (
     <Fragment>
@@ -47,11 +60,26 @@ const Messages: React.FC = () => {
         <button type="submit">New Message</button>
       </form>
       <List
+        className="demo-loadmore-list"
+        loading={loading}
         itemLayout="horizontal"
         dataSource={data.messages}
         renderItem={(item: any) => (
-          <List.Item>
-            <List.Item.Meta title={<a href="https://ant.design">{item.id}</a>} description={item.description} />
+          <List.Item
+            actions={[
+              <a key="list-loadmore-edit">edit</a>,
+              <a key="list-loadmore-delete" onClick={() => handleClickDelete(item.id)}>
+                delete
+              </a>
+            ]}
+          >
+            <Skeleton avatar title={false} loading={item.loading} active>
+              <List.Item.Meta
+                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                title={<a href="https://ant.design">{item.id}</a>}
+                description={item.description}
+              />
+            </Skeleton>
           </List.Item>
         )}
       />
